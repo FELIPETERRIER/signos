847dfae4dd7fdebac9e6dbac37d221487f135213
Felipe Bitencourt <Felipeterrier@yahoo.com.br>
2024-01-10 13:39:18 -0300

    first commit

---
+++ signos/src/App.js
@@ -0,0 +1,12 @@
+import ListaDeSignos from './componentes/listaSignos';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ListaDeSignos/>
+    </div>
+  );
+}
+
+export default App;
---
+++ signos/src/componentes/titulo/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+import './style.css';
+
+export default function Titulo() {
+  return (
+    <div className='box'>
+      <p className='texto'>Baralho de signos</p>
+      
+    </div>
+  );
+}
+
+ 
